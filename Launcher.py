0f2f3d3343ae0d99e5238e1c5c4a85daaee1308f
RedBirdFish <ectrout@ilstu.edu>
2025-04-28 18:20:09 -0500

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -7,11 +7,11 @@ def main():
     current_os = platform.system()
     if current_os == "Windows":
         print("🔍 Detected Windows. Running Batch Forensics Tool...")
-        subprocess.run(["forensic_tools.bat"], shell=True)
+        subprocess.call(["forensic_tools.bat"], shell=True)
     elif current_os == "Linux" or current_os == "Darwin":
         print("🔍 Detected Unix-based OS. Running Bash Forensics Tool...")
-        subprocess.run(["chmod", "+x", "forensic_tools.bash"])
-        subprocess.run(["./forensic_tools.bash"])
+        subprocess.call(["bash" ,"sudo", "chmod", "+x", "forensic_tools.sh"])
+        subprocess.call(["bash", "./forensic_tools.sh"])
     else:
         print("❌ Unsupported OS.")
         sys.exit(1)
